3d91add8fb7671bd6b1458002b0508c4c24f95b0
bill6300gp <bill6300gp@hotmail.com>
2018-02-25 04:07:55 +0800

    Update: Installation for RuneAudio
    Update: Installation for RuneAudio

--- setup.sh
+++ setup.sh
@@ -1,34 +1,88 @@
 #! /bin/bash
 
-if [ -f /etc/init.d/playerdisp ]; then
-  echo "playerdisp file has been upload."
-else
-  echo "Installing the playerdisp..."
-  sudo cp playerdisp /etc/init.d/
-  sudo chmod 755 /etc/init.d/playerdisp
-  sudo update-rc.d playerdisp defaults
+echo "Install playerdisp script"
+echo "-------------------------"
+
+# Modify file path
+locate=`pwd`
+#echo "The scripts are in the path: $locate"
+if [ -f $locate/playerdisp ]; then
+	sed -i "s|{absolute_path}|$locate|g" playerdisp
+fi
+if [ -f $locate/playerdisp.service ]; then
+	sed -i "s|{absolute_path}|$locate|g" playerdisp.service
 fi
 
+# Check Libraries
+echo "Check Libraries:"
 tmp=`python2 -m smbus |& grep -q 'No module named' && echo 0 || echo 1`
+count=0
 if [ $tmp -eq 1 ]; then
-  echo "smbus module has been installed."
+	echo -e "[\033[0;32m OK \033[0m] python smbus module has been installed."
+	count=$(($count+1))
 else
-  echo "Installing python-smbus module..."
-  sudo apt-get update
-  sudo apt-get install python-smbus
+	echo -e "[\033[0;31mFail\033[0m] without python smbus module."
+fi
+tmp=`python2 -m RPi.GPIO |& grep -q 'No module named' && echo 0 || echo 1`
+if [ $tmp -eq 1 ]; then
+	echo -e "[\033[0;32m OK \033[0m] RPi.GPIO package has been installed."
+	count=$(($count+1))
+else
+	tmp=`python2 -m RPi.GPIO.__init__ |& grep -q 'No module named' && echo 0 || echo 1`
+	if [ $tmp -eq 1 ]; then
+		echo -e "[\033[0;32m OK \033[0m] RPi.GPIO package has been installed."
+		count=$(($count+1))
+	else
+		echo -e "[\033[0;31mFail\033[0m] without RPi.GPIO package."
+	fi
+fi
+echo "-----------------------------------------------"
+
+# Install and run
+mps=`uname -n`
+if [ "$mps" == "volumio" ]; then
+	echo -e "Music Player System is \033[0;33mVolumio\033[0m."
+	if [ -f /etc/init.d/playerdisp ]; then
+		echo "playerdisp file has been upload."
+	else
+		echo "Installing the playerdisp..."
+		sudo cp playerdisp /etc/init.d/
+		sudo chmod 755 /etc/init.d/playerdisp
+		sudo update-rc.d playerdisp defaults
+	fi
+
+	PID=`ps -ef | grep show.py | grep -v grep | awk '{print $2}'`
+	if [ -z $PID ]; then
+		if [ $count -eq 2 ]; then
+			sudo /etc/init.d/playerdisp start
+		fi
+	#else
+	#	echo "playerdisp has run already."
+	fi
+elif [ "$mps" == "runeaudio" ]; then
+	echo -e "Music Player System is \033[0;33mRuneAudio\033[0m."
+	tmp=`systemctl status playerdisp |& grep -q 'No such file' && echo 0 || echo 1`
+	if [ $tmp -eq 0 ]; then
+		echo "install playerdisp script..."
+		chmod 755 playerdisp
+		cp playerdisp.service /usr/lib/systemd/system/
+		systemctl daemon-reload
+		systemctl enable playerdisp
+	else
+		echo "playerdisp has been installed already."
+	fi
+
+	stat=`systemctl status playerdisp |& grep -q 'inactive' && echo 0 || echo 1`
+	if [ $stat -eq 0 ]; then
+		if [ $count -eq 2 ]; then
+			echo "Start playerdisp..."
+			systemctl start playerdisp
+		fi
+	#else
+	#	echo "playerdisp has run already."
+	fi
+else
+	echo "Unknow system. Stopped installing!"
 fi
 
-tmp=` python2 -m RPi.GPIO.__init__ |& grep -q 'No module named' && echo 0 || echo 1`
-if [ $tmp -eq 1 ]; then
-  echo "RPi.GPIO package has been installed."
-else
-  echo "Installing rpi.gpio package..."
-  sudo apt-get install rpi.gpio
-fi
 
-PID=`ps -ef | grep show.py | grep -v grep | awk '{print $2}'`
-if [ -z $PID ]; then
-  sudo /etc/init.d/playerdisp start
-else
-  echo "playerdisp has run already."
-fi
